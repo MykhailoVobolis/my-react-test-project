8b0f16bda490f5c3d672904a3baf545cb6045496
MykhailoVobolis <mvobolis@gmail.com>
2024-03-26 11:31:40 +0200

    my second commit

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,20 +2,14 @@ import clsx from "clsx";
 
 import css from "./Alert.module.css";
 
-export const Alert = ({ variant, children }) => {
-  return <p className={clsx(css.alert, css[variant])}>{children}</p>;
+export const Alert = ({ variant, outlined, elevated, children }) => {
+  return (
+    <p
+      className={clsx(css[variant], {
+        [css.isOutlined]: outlined,
+        [css.isElevated]: elevated,
+      })}>
+      {children}
+    </p>
+  );
 };
-
-// export const Alert = ({ variant, outlined, elevated, children }) => {
-//   const classNames = ["alert", variant];
-
-//   return (
-//     <p
-//       className={clsx("alert", variant, {
-//         "is-outlined": outlined,
-//         "is-elevated": elevated,
-//       })}>
-//       {children}
-//     </p>
-//   );
-// };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,5 @@
-// import { BookList } from './Booklist';
-
-// const favouriteBooks = [
-//   { id: "id-1", name: "JS for beginners" },
-//   { id: "id-2", name: "React basics" },
-//   { id: "id-3", name: "React Router overview" }
-// ];
-
-// export default function App() {
-//   return (
-//     <>
-//     	<h1>Books of the week</h1>
-//       <BookList books={favouriteBooks} />
-//     </>
-//   );
-// }
-
 import { Alert } from "./Alert";
+import { UserMenu } from "./UserMenu";
 
 export default function App() {
   return (
@@ -30,6 +14,7 @@ export default function App() {
       <Alert variant="warning" outlined elevated>
         Please update your profile contact information
       </Alert>
+      <UserMenu name="Mango" />
     </>
   );
 }
