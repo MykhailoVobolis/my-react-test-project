4d7ea5b1f89edcb1f24067146cf9f8e704ea97d0
MykhailoVobolis <mvobolis@gmail.com>
2024-03-30 14:19:04 +0200

    my second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,30 @@
-import { Alert } from "./Alert";
-import { UserMenu } from "./UserMenu";
+// import Modal from "./Modal";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [clicks, setClicks] = useState(() => {
+    // Зчитуємо значення за ключем
+    const savedClicks = window.localStorage.getItem("saved-clicks");
+
+    // Якщо там щось є, повертаємо це
+    // значення як початкове значення стану
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+
+    // У протилежному випадку повертаємо
+    // яке-небудь значення за замовчуванням
+    return 0;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-clicks", clicks);
+  }, [clicks]);
+
   return (
-    <>
-      <Alert variant="info">Would you like to browse our recommended products?</Alert>
-      <Alert variant="error" outlined>
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success" elevated>
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning" outlined elevated>
-        Please update your profile contact information
-      </Alert>
-      <UserMenu name="Mango" />
-    </>
+    <div>
+      <button onClick={() => setClicks(clicks + 1)}>You clicked {clicks} times</button>
+      <button onClick={() => setClicks(0)}>Reset</button>
+    </div>
   );
 }
